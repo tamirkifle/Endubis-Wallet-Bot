7e6169665d52f881846459440061867a1f4e507b
Tamir K. Yirga <tkifle@gmail.com>
2022-03-02 09:55:50 +0300

    Sync progress when sending before wallet syncs

--- scenes/sendScene.js
+++ scenes/sendScene.js
@@ -6,7 +6,22 @@ const {
 } = require("../utils/btnMenuHelpers");
 const { getWalletById } = require("../utils/loadAccount");
 
-const step1 = (ctx) => {
+const step1 = async (ctx) => {
+  const wallet = await getWalletById(ctx.session.loggedInWalletId);
+  if (wallet.state.status !== "ready") {
+    await ctx.deleteMessage();
+    await ctx.reply(
+      `Your wallet is syncing.
+Please Wait for it to finish before trying to send...
+        
+Progress: ${wallet.state.progress.quantity} ${wallet.state.progress.unit}`,
+      Markup.inlineKeyboard([
+        [Markup.button.callback("Refresh", "send")],
+        [mainMenuButton()],
+      ])
+    );
+    return;
+  }
   ctx.reply(
     "Please choose an option from the choices below",
     Markup.inlineKeyboard([
