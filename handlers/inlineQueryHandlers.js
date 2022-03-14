d4e362f7a065027ef46493f8aca587df9b8dcc11
Tamir K. Yirga <tkifle@gmail.com>
2022-03-14 09:28:47 +0300

    used wallet data not sender for receiving links

--- handlers/inlineQueryHandlers.js
+++ handlers/inlineQueryHandlers.js
@@ -1,5 +1,5 @@
 const { Markup } = require("telegraf");
-const { getReceivingAddress } = require("../utils/loadAccount");
+const { getReceivingAddress, getWalletById } = require("../utils/loadAccount");
 const { generateQrFileId } = require("../utils/qrCodeHerlper");
 
 const sHandler = (ctx) => {
@@ -10,7 +10,15 @@ const sHandler = (ctx) => {
   });
 };
 const qrHandler = async (ctx) => {
-  const startPayload = Buffer.from(`sendto=${ctx.from.id}`).toString("base64");
+  if (!ctx.session?.loggedInWalletId) {
+    // Handle new users using inline mode
+    return;
+  }
+  const wallet = await getWalletById(ctx.session.loggedInWalletId);
+  const bot = require("../botSession");
+  const chat = await bot.telegram.getChat(wallet.name);
+
+  const startPayload = Buffer.from(`sendto=${chat.id}`).toString("base64");
   const file_id = await generateQrFileId(
     ctx,
     `http://t.me/Testing_TM_Bot?start=${startPayload}`
@@ -23,7 +31,7 @@ const qrHandler = async (ctx) => {
       title: `Send a payment QR Code`,
       description:
         "Send a message with a QR Code for receiving payments to your address",
-      caption: `Send to <b>@${ctx.from.username}</b>(<b>${ctx.from.first_name} ${ctx.from.last_name}</b>)`,
+      caption: `Send to <b>@${chat.username}</b>(<b>${chat.first_name} ${chat.last_name}</b>)`,
       parse_mode: "HTML",
     },
   ];
@@ -31,8 +39,16 @@ const qrHandler = async (ctx) => {
 };
 
 const generalInlineHandler = async (ctx) => {
+  if (!ctx.session?.loggedInWalletId) {
+    // Handle new users using inline mode
+    return;
+  }
+  const wallet = await getWalletById(ctx.session.loggedInWalletId);
+  const bot = require("../botSession");
+  const chat = await bot.telegram.getChat(wallet.name);
+
   const address = await getReceivingAddress(ctx.session?.loggedInWalletId);
-  const startPayload = Buffer.from(`sendto=${ctx.from.id}`).toString("base64");
+  const startPayload = Buffer.from(`sendto=${chat.id}`).toString("base64");
   const results = [
     {
       type: "article",
@@ -51,7 +67,7 @@ const generalInlineHandler = async (ctx) => {
       description:
         "Send a message with a link to the wallet with your contact pre-filled",
       input_message_content: {
-        message_text: `Send to <b>@${ctx.from.username}</b> (<b>${ctx.from.first_name} ${ctx.from.last_name}</b>)`,
+        message_text: `Send to <b>@${chat.username}</b> (<b>${chat.first_name} ${chat.last_name}</b>)`,
         parse_mode: "HTML",
       },
       ...Markup.inlineKeyboard([
@@ -68,10 +84,18 @@ const generalInlineHandler = async (ctx) => {
 };
 
 const generalWithAmountHandler = async (ctx) => {
+  if (!ctx.session?.loggedInWalletId) {
+    // Handle new users using inline mode
+    return;
+  }
+  const wallet = await getWalletById(ctx.session.loggedInWalletId);
+  const bot = require("../botSession");
+  const chat = await bot.telegram.getChat(wallet.name);
+
   if (Number(ctx.match[1])) {
     const amountToSend = Number(ctx.match[1]);
     const startPayload = Buffer.from(
-      `sendto=${ctx.from.id}&amount=${amountToSend}&expiry=${Date.now()}`
+      `sendto=${chat.id}&amount=${amountToSend}&expiry=${Date.now()}`
     ).toString("base64");
     const address = await getReceivingAddress(ctx.session?.loggedInWalletId);
     const results = [
@@ -92,14 +116,14 @@ const generalWithAmountHandler = async (ctx) => {
         description:
           "Send a message with a link to the wallet with your contact pre-filled",
         input_message_content: {
-          message_text: `<b>@${ctx.from.username}</b> (<b>${ctx.from.first_name} ${ctx.from.last_name}</b>) has requested <i><b>${amountToSend} ada</b></i>
+          message_text: `<b>@${chat.username}</b> (<b>${chat.first_name} ${chat.last_name}</b>) has requested <i><b>${amountToSend} ada</b></i>
 <i>Note: The payment button below expires after one hour</i>`,
           parse_mode: "HTML",
         },
         ...Markup.inlineKeyboard([
           [
             Markup.button.url(
-              `Send ${amountToSend} ada to @${ctx.from.username}`,
+              `Send ${amountToSend} ada to @${chat.username}`,
               `http://t.me/Testing_TM_Bot?start=${startPayload}`
             ),
           ],
