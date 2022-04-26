a61a0eca334fd2eca4cfeeb15e337dbd80913348
Tamir K. Yirga <41894752+tamirkifle@users.noreply.github.com>
2022-04-26 14:07:45 +0300

    updated walletUtils URL to check .env file

--- utils/walletUtils.js
+++ utils/walletUtils.js
@@ -7,7 +7,9 @@ const {
   WalletsApi,
 } = require("cardano-wallet-js/dist/api");
 
-let walletServer = WalletServer.init("http://localhost:8090/v2");
+let walletServer = WalletServer.init(
+  process.env.WALLET_SERVER_URL || "http://localhost:8090/v2"
+);
 
 const loadAccountFromSeed = async (seedPhrases, passphrase, walletName) => {
   const seedArray = Seed.toMnemonicList(seedPhrases);
