c1c68a4af227620c8769cd64c64579a10f563788
Tamir K. Yirga <41894752+tamirkifle@users.noreply.github.com>
2022-05-10 16:48:33 +0300

    added back in transaction scene menus

--- scenes/viewTransactionsScene.js
+++ scenes/viewTransactionsScene.js
@@ -5,8 +5,8 @@ const { formatTxnData } = require("../utils/formatTxnData");
 const { getTransactions } = require("../utils/newWalletUtils");
 const { getWalletById } = require("../utils/walletUtils");
 
-const step1 = (ctx) => {
-  ctx.reply(
+const transactionHandlerStart = async (ctx) => {
+  await ctx.reply(
     `Please choose a filter option to see your list of transactions`,
     Markup.inlineKeyboard([
       [Markup.button.callback("By Time Frame 🕒", "txns-by-time")],
@@ -18,6 +18,100 @@ const step1 = (ctx) => {
   );
   return ctx.wizard.next();
 };
+
+const txnListHandler =
+  ({ months: monthsOfTxns = undefined, direction } = {}) =>
+  async (ctx) => {
+    // const wallet = await getWalletById(ctx.session.xpubWalletId);
+    let wallet;
+    try {
+      wallet = await getWalletById(ctx.session.xpubWalletId);
+    } catch (error) {
+      wallet = null;
+    }
+    let txns;
+    if (monthsOfTxns) {
+      const oneMonthInMS = 2629800000;
+      const endDate = new Date(Date.now());
+      const startDate = new Date(Date.now() - oneMonthInMS * monthsOfTxns);
+      if (wallet?.state?.status === "ready") {
+        txns = await wallet.getTransactions(startDate, endDate);
+      } else {
+        txns = await getTransactions(ctx);
+      }
+    } else if (direction) {
+      if (wallet?.state?.status === "ready") {
+        txns = await wallet.getTransactions();
+      } else {
+        txns = await getTransactions(ctx);
+      }
+      txns = txns.filter(
+        (txn) => txn.direction === String(direction).toLowerCase()
+      );
+    } else {
+      if (wallet?.state?.status === "ready") {
+        txns = await wallet.getTransactions();
+      } else {
+        txns = await getTransactions(ctx);
+      }
+    }
+    const customInlineKeyboard = (index, txnId) => {
+      if (index === txns.length - 1) {
+        return Markup.inlineKeyboard([
+          [
+            Markup.button.url(
+              "More Details",
+              `https://testnet.cardanoscan.io/transaction/${txnId}`
+            ),
+          ],
+          [Markup.button.callback("Go Back", "go-back")],
+          [mainMenuButton()],
+        ]);
+      }
+      return Markup.inlineKeyboard([
+        [
+          Markup.button.url(
+            "More Details",
+            `https://testnet.cardanoscan.io/transaction/${txnId}`
+          ),
+        ],
+      ]);
+    };
+    if (txns.length === 0) {
+      console.log(ctx.wizard.cursor);
+      await ctx.replyWithHTML(
+        `No Transactions to Show`,
+        Markup.inlineKeyboard([
+          [
+            ctx.wizard.cursor === 1
+              ? Markup.button.callback("Go Back", "txns-by-time")
+              : Markup.button.callback("Go Back", "go-back"),
+          ],
+          [mainMenuButton()],
+        ])
+      );
+    }
+    const txnMessages = [];
+    for (const [index, txn] of txns.entries()) {
+      const { message_id } =
+        (await ctx.replyWithHTML(
+          `${index === 0 ? "Click More Details to see more." : ""}
+  
+<b>#${txns.length - index} of ${txns.length} Transactions</b>
+${formatTxnData(txn)}`,
+          customInlineKeyboard(index, txn.id)
+        )) || {};
+      message_id && txnMessages.push(message_id);
+    }
+    ctx.session.messageIdsToDelete = [
+      ...txnMessages,
+      ...(ctx.session.messageIdsToDelete || []),
+    ];
+    return ctx.wizard.next();
+  };
+
+const step1 = new Composer();
+step1.use(transactionHandlerStart);
 
 /* 
 Step 1
@@ -32,7 +126,12 @@ Step 1
 */
 
 const step2 = new Composer();
-step2.action("txns-by-time", (ctx) => {
+step2.action("go-back", async (ctx) => {
+  ctx.wizard.back();
+  return transactionHandlerStart(ctx);
+});
+
+const txnByTimeHandler = async (ctx) => {
   ctx.reply(
     `Choose time frame to see transactions`,
     Markup.inlineKeyboard([
@@ -42,19 +141,17 @@ step2.action("txns-by-time", (ctx) => {
       [Markup.button.callback("Past 6 months", "txns-past-6-months")],
       [Markup.button.callback("Past year", "txns-past-year")],
       [Markup.button.callback("All time", "txns-all-time")],
+      [Markup.button.callback("Go Back", "go-back")],
       [mainMenuButton()],
     ])
   );
   return ctx.wizard.next();
-});
+};
+step2.action("txns-by-time", txnByTimeHandler);
 
-step2.action("txns-sent", (ctx) =>
-  txnListHandler(ctx, { direction: "outgoing" })
-);
-step2.action("txns-received", (ctx) =>
-  txnListHandler(ctx, { direction: "incoming" })
-);
-step2.action("txns-all", (ctx) => txnListHandler(ctx));
+step2.action("txns-sent", txnListHandler({ direction: "outgoing" }));
+step2.action("txns-received", txnListHandler({ direction: "incoming" }));
+step2.action("txns-all", txnListHandler());
 
 /* 
 Step 2
@@ -64,92 +161,30 @@ Step 2
   - Ask for start date
 */
 
-const txnListHandler = async (
-  ctx,
-  { months: monthsOfTxns = undefined, direction } = {}
-) => {
-  // const wallet = await getWalletById(ctx.session.xpubWalletId);
-  let wallet;
-  try {
-    wallet = await getWalletById(ctx.session.xpubWalletId);
-  } catch (error) {
-    wallet = null;
-  }
-  let txns;
-  if (monthsOfTxns) {
-    const oneMonthInMS = 2629800000;
-    const endDate = new Date(Date.now());
-    const startDate = new Date(Date.now() - oneMonthInMS * monthsOfTxns);
-    if (wallet?.state?.status === "ready") {
-      txns = await wallet.getTransactions(startDate, endDate);
-    } else {
-      txns = await getTransactions(ctx);
-    }
-  } else if (direction) {
-    if (wallet?.state?.status === "ready") {
-      txns = await wallet.getTransactions();
-    } else {
-      txns = await getTransactions(ctx);
-    }
-    txns = txns.filter(
-      (txn) => txn.direction === String(direction).toLowerCase()
-    );
-  } else {
-    if (wallet?.state?.status === "ready") {
-      txns = await wallet.getTransactions();
-    } else {
-      txns = await getTransactions(ctx);
-    }
-  }
-  const customInlineKeyboard = (index, txnId) => {
-    if (index === txns.length - 1) {
-      return Markup.inlineKeyboard([
-        [
-          Markup.button.url(
-            "More Details",
-            `https://testnet.cardanoscan.io/transaction/${txnId}`
-          ),
-        ],
-        [mainMenuButton()],
-      ]);
-    }
-    return Markup.inlineKeyboard([
-      [
-        Markup.button.url(
-          "More Details",
-          `https://testnet.cardanoscan.io/transaction/${txnId}`
-        ),
-      ],
-    ]);
-  };
-
-  for (const [index, txn] of txns.entries()) {
-    await ctx.replyWithHTML(
-      `${index === 0 ? "Click More Details to see more." : ""}
-  
-<b>#${txns.length - index} of ${txns.length} Transactions</b>
-${formatTxnData(txn)}`,
-      customInlineKeyboard(index, txn.id)
-    );
-  }
-  return;
-};
-
 const step3 = new Composer();
-step3.action("txns-all-time", (ctx) => txnListHandler(ctx));
-step3.action("txns-past-week", (ctx) =>
-  txnListHandler(ctx, { months: 7 / 30 })
-);
-step3.action("txns-past-month", (ctx) => txnListHandler(ctx, { months: 1 }));
-step3.action("txns-past-3-months", (ctx) => txnListHandler(ctx, { months: 3 }));
-step3.action("txns-past-6-months", (ctx) => txnListHandler(ctx, { months: 6 }));
-step3.action("txns-past-year", (ctx) => txnListHandler(ctx, { months: 12 }));
+step3.action("go-back", async (ctx) => {
+  ctx.wizard.selectStep(0);
+  return transactionHandlerStart(ctx);
+});
+step3.action("txns-all-time", txnListHandler());
+step3.action("txns-past-week", txnListHandler({ months: 7 / 30 }));
+step3.action("txns-past-month", txnListHandler({ months: 1 }));
+step3.action("txns-past-3-months", txnListHandler({ months: 3 }));
+step3.action("txns-past-6-months", txnListHandler({ months: 6 }));
+step3.action("txns-past-year", txnListHandler({ months: 12 }));
+
+const step4 = new Composer();
+step4.action("go-back", async (ctx) => {
+  ctx.wizard.selectStep(1);
+  return txnByTimeHandler(ctx);
+});
 
 const viewTransactionsScene = new Scenes.WizardScene(
   "viewTransactionsScene",
   step1,
   step2,
-  step3
+  step3,
+  step4
 );
 
 module.exports = { viewTransactionsScene };
